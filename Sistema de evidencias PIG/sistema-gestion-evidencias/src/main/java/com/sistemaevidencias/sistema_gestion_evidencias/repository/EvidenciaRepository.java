fd9e21938e21eafc23c694a93d936ad8e3dcb6b0
zerogptnew-source <zerogptnew@gmail.com>
2026-07-21 21:02:21 -0500

    Delete Sistema de evidencias PIG/sistema-gestion-evidencias directory

--- Sistema de evidencias PIG/sistema-gestion-evidencias/src/main/java/com/sistemaevidencias/sistema_gestion_evidencias/repository/EvidenciaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sistemaevidencias.sistema_gestion_evidencias.repository;
-
-import com.sistemaevidencias.sistema_gestion_evidencias.model.Evidencia;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EvidenciaRepository extends JpaRepository<Evidencia, Long> {
-}
